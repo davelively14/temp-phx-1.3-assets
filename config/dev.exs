d5926f5efd5f9f021cd0e2865856de1e285075cd
Dave Lively <davelively14@gmail.com>
2017-03-29 13:10:58 -0400

    Configured

--- config/dev.exs
+++ config/dev.exs
@@ -11,7 +11,9 @@ config :temp, Temp.Web.Endpoint,
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
-  watchers: []
+  watchers: [
+    node: ["node_modules/webpack/bin/webpack.js", "--watch-stdin", "--color", cd: Path.expand("../assets", __DIR__)]
+  ]
 
 # ## SSL Support
 #
